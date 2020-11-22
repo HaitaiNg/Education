332f12989c6d998a15cbbf48dcd84b44b05c1c83
haitai <hkng248@gmail.com>
2020-11-21 23:32:20 -0500

    LC

--- Ethic_Coding_Exercise/Ethic_Coding_Exercise.java
+++ Ethic_Coding_Exercise/Ethic_Coding_Exercise.java
@@ -1,40 +1,91 @@
-
 import java.util.*; 
-import java.util.Collections; 
 
 public class Ethic_Coding_Exercise {
+    public Graph graph; 
 
-    public static void sample_input()
-    {
-        String routes = "AB5,BC4,CD8,DC8,DE6,AD5,CE2,EB3,AE7"; 
-        String[] values = routes.split(","); 
-        List<String> listOfRoutes = Arrays.asList(values); 
-        Graph graph = graph(listOfRoutes);
+    public Ethic_Coding_Exercise(){
+        System.out.println("Graph Has Been Initialized"); 
+        graph = new Graph(); 
     }
 
-    // javac *.java
-    public static void main(String[] args)
+    public void createGraph(List<String> routes)
     {
-        sample_input();
-        //1 
-        // System.out.println(graph.calculateDistanceAlongRoute("ABC"));
-        //4 
-        //  System.out.println(graph.dijkstrasAlgorithm(graph, "E", "A")); 
-        //  System.out.println(graph.dijkstrasAlgorithm(graph, "A", "C"));
+        // Trim all leading  & trailing white spaces
+        List<String> trimRoutes = new ArrayList<String>(); 
+        for(String route: routes)
+        {
+            trimRoutes.add(route.trim()); 
+        }
+        routes = trimRoutes; 
 
-        //3 
-        // System.out.println(graph.getRoutesWithLimitedPaths(graph, "A", "D", 10));
-        // System.out.println(graph.getRoutesWithLimitedPaths(graph, "C", "C", 10));
+        // Create Nodes 
+        for(char id: getDistinctNodes(routes)){
+            graph.addNode(id); 
+        }
+        // Create Edges 
+        for(String route: routes){
+            char start = route.charAt(0);
+            char end = route.charAt(1); 
+            int weight = Integer.parseInt(String.valueOf(route.substring(2, route.length())));
+            graph.addEdge(start, end, weight);
+        }
+    }
 
-        // System.out.println(graph.getRoutesWithLimitedDistance(graph, "C", "C", 20)); 
-        // System.out.println(graph.getRoutesWithLimitedDistance(graph, "C", "C", 10));
-        // e
+    public Set<Character> getDistinctNodes(List<String> listOfNodes)
+    {
+        Set<Character> distinctNodes = new HashSet<Character>(); 
+        for(String node: listOfNodes)
+        {
+            char start = node.charAt(0);
+            char end = node.charAt(1); 
+            if(!distinctNodes.contains(start)) distinctNodes.add(start); 
+            if(!distinctNodes.contains(end)) distinctNodes.add(end); 
+        }
+        return distinctNodes; 
+    }
+
+    // Question 1 : Return distance along route 
+    public String getDistanceAlongRoute(String route){
+        printDistanceAlongRoute(route);
+        return graph.calculateDistanceAlongRoute(route); 
+    }
+
+    // Question 2 : Return the number of routes with limited # of stops 
+    public String getRoutesWithLimitedStops(String start, String end, int maxNumberOfStops)
+    {
+        printRoutesWithLimitedStops(start, end, maxNumberOfStops);
+        return String.valueOf(graph.getRoutesLimitedStops(graph, start, end, maxNumberOfStops));
+    }
+
+    // Question 3 : Return the number of routes with a distance less than max 
+    public String getRoutesWithLimitedDistance(String start, String end, int maxDistance)
+    {
+        printRoutesWithLimitedDistance(start, end, maxDistance);
+        return String.valueOf(graph.getRoutesLimitedDistance(graph, start, end, maxDistance));
+    }
+
+    // Question 4 : Return the shortest route between two towns 
+    public String getShortestRouteBetweenTwoTowns(String start, String end){
+        printGetShortestRouteBetweenTwoTowns(start, end);
+        return graph.dijkstrasAlgorithm(graph, start, end); 
     }
 
 
-
-
-
-
-    
+    //< All print statements 
+    public void printDistanceAlongRoute(String route){
+        if(route == "\0") System.out.println(("The distance of the route _").trim()); 
+        else System.out.println(("The distance of the route " + route).trim()); 
+    }
+    public void printRoutesWithLimitedStops(String start, String end, int maxNumberOfStops){
+        if(start == "\0" || end == "\0") System.out.println("The number of trips starting at _ and ending at _ with a maximum of _ stops");
+        else System.out.println("The number of trips starting at " + start + " and ending at " + end + " with a maximum of " + String.valueOf(maxNumberOfStops) + " stops");
+    }
+    public void printRoutesWithLimitedDistance(String start, String end, int maxDistance){
+        if(start == "\0" || end == "\0") System.out.println("The number of different routes from _ to _ with a distance of less than _");
+        else System.out.println("The number of different routes from " + start + " to " + end + " with a distance of less than " + String.valueOf(maxDistance));
+    }
+    public void printGetShortestRouteBetweenTwoTowns(String start, String end){
+        if(start == "\0" || end == "\0") System.out.println("The length of the shortest route (in terms of distance to travel) from _ to _");
+        else System.out.println("The length of the shortest route (in terms of distance to travel) from " + start + " to " + end);
+    }
 }
--- Ethic_Coding_Exercise/Graph.java
+++ Ethic_Coding_Exercise/Graph.java
@@ -1,18 +1,19 @@
-package Ethic_Coding_Exercise;
 
 
 import java.util.*;
 
-public class Graph{
+class Graph{
 
+    // Node class definition 
     class Node{
+        
         char id; 
         Node(char ID)
         {
             this.id = ID; 
         }
-        //Dijkstras algorithm is here 
-        private int distance = Integer.MAX_VALUE; //< this is used for Dijkstra's algorithm 
+
+        private int distance = Integer.MAX_VALUE;
         public int getDistance(){
             return distance; 
         }
@@ -26,30 +27,18 @@ public class Graph{
         public void setShortestPath(LinkedList<Node> shortestPath){
             this.shortestPath = shortestPath;}
     }
-    
+    // end Node class definition 
+
+
+    // Graph member variables and methods 
     public Map<Node, Map<Node, Integer>> adjacencyList; 
     public List<String> allPathsBetweenTwoNodes; 
-
-
     public Graph(){
         adjacencyList = new HashMap<Node, Map<Node, Integer>>(); 
         allPathsBetweenTwoNodes = new ArrayList<String>(); 
     }
-    
-    // Add Node 
-    void addNode(Character id)
-    {
-        adjacencyList.putIfAbsent(new Node(id), new HashMap<Node, Integer>()); 
-    }
-    // Add edge & connect nodes 
-    void addEdge(Character idOne, Character idTwo, int weight)
-    {
-        // Simplified approach 
-        // Node n1 = new Node(idOne);
-        // Node n2 = new Node(idTwo); 
-        // adjacencyList.get(n1).put(n2, weight); //< this throws a compile error 
-        // ignoring weights for now 
-        
+
+    void addEdge(Character idOne, Character idTwo, int weight){
         Node n2 = new Node(idTwo); 
         for(Node node: adjacencyList.keySet())
         {
@@ -59,6 +48,31 @@ public class Graph{
         } 
     }
 
+    void addNode(Character id){
+        adjacencyList.putIfAbsent(new Node(id), new HashMap<Node, Integer>()); 
+    }
+
+    public void addPath(Graph graph, List<Character> visited)
+    {
+        String path = "";
+        for(char node : visited){
+            path += String.valueOf(node);
+        }
+        graph.allPathsBetweenTwoNodes.add(path.trim()); 
+    }
+
+    //Get vertices for a particular node 
+    Map<Node, Integer> getAdjacentVerticies(char id){
+        for(Node node: adjacencyList.keySet())
+        {
+            if(node.id == id)
+            {
+                return adjacencyList.get(node);
+            }
+        }
+        return null;
+    }
+
     Node getNode(String nodeName){
         for(Node node: adjacencyList.keySet()){
             if(node.id == nodeName.charAt(0)) return node; 
@@ -66,24 +80,93 @@ public class Graph{
         return null; 
     }
 
-
-
-    //Get vertices for a particular node 
-    Map<Node, Integer> getAdjacentVerticies(char id)
+    // Get all possible paths from start node to end node 
+    public void calculateAllPaths(Graph graph, String start, String end)
     {
-        for(Node c: adjacencyList.keySet())
-        {
-            if(c.id == id)
-            {
-                return adjacencyList.get(c);
-            }
+        List<Character> visited = new ArrayList<Character>(); 
+        visited.add(start.charAt(0));
+        //if start node & end node are the same, get the start node's adjacent verticies and find all paths from 
+        // the start node's adjacent nodes to the end, and return those
+        if(start == end){
+            Map<Node, Integer> adjacentNodes = graph.getAdjacentVerticies(start.charAt(0));
+            for(Node node: adjacentNodes.keySet()){
+                List<Character> visitedTemp = new ArrayList<Character>(); 
+                visitedTemp.add(node.id);
+                search(graph, String.valueOf(node.id), end, visitedTemp);
+            }; 
         }
-        return null;
+        else{
+            search(graph, start, end, visited);
+        }
     }
 
-    // Part One 
-    int getDistanceAlongRoute(String route)
+    public Node calculateClosestNode(Set<Node> unvisited){
+        Node closestNode = null; 
+        int closestDistance = Integer.MAX_VALUE; 
+        for(Node node: unvisited){
+            int nodeDistance = node.getDistance();
+            if( nodeDistance< closestDistance)
+            {
+                closestDistance = nodeDistance;
+                closestNode = node; 
+            }
+        }
+        return closestNode; 
+    }
+
+    public void calculateShortestDistance(Graph graph, Node adjacentNode, Node source, int weight){
+        if(source.getDistance() + weight < adjacentNode.getDistance()){
+            adjacentNode.setDistance(source.getDistance()+weight);
+            //System.out.println(adjacentNode.getDistance());
+
+            Node temp = graph.getNode(String.valueOf(adjacentNode.id));
+            if(adjacentNode.getDistance() < temp.getDistance())
+            {
+                temp.setDistance(adjacentNode.getDistance());
+            }
+            LinkedList<Node> shortestPath = new LinkedList<>(source.getShortestPath()); 
+            shortestPath.add(source); 
+            adjacentNode.setShortestPath(shortestPath);
+        }
+    }
+
+    // Helper method to calculate the shortest route between two towns 
+    public String dijkstrasAlgorithm(Graph graph, String start, String end)
     {
+        Node startNode = graph.getNode(start); 
+        startNode.setDistance(0); 
+        Set<Node> visited = new HashSet<Node>(); 
+        Set<Node> unvisited = new HashSet<Node>(); 
+        unvisited.add(startNode); 
+        while(unvisited.size() > 0){
+            Node currentNode = calculateClosestNode(unvisited); 
+            unvisited.remove(currentNode);
+            for(Map.Entry<Node, Integer> adjacentNode: graph.getAdjacentVerticies(currentNode.id).entrySet()){
+                if(!visited.contains(adjacentNode.getKey())){
+                    //< adjacentNode.getKey() = node
+                    //< adjacentNode.getValue() = node weight 
+                    calculateShortestDistance(graph, adjacentNode.getKey(), currentNode, adjacentNode.getValue()); 
+                    unvisited.add(adjacentNode.getKey()); 
+                }
+            }
+             visited.add(currentNode);    
+         }
+         int shortestDistance = graph.getNode(end).getDistance(); 
+         if(shortestDistance == Integer.MAX_VALUE) return("NO SUCH ROUTE").trim();
+        return String.valueOf(graph.getNode(end).getDistance());
+    }
+
+
+    public String calculateDistanceAlongRoute(String route)
+    {
+        int distanceAlongRoute  = getDistanceAlongRoute(route);
+        if(distanceAlongRoute == 0) return("NO SUCH ROUTE").trim();
+        else return (String.valueOf(distanceAlongRoute)).trim();
+    }
+
+
+    // Return the distance along a certain route 
+    int getDistanceAlongRoute(String route){
         int costOfRoute = 0; 
         int slow = 0; int fast = 1; 
         while( fast < route.length()){
@@ -100,172 +183,70 @@ public class Graph{
         return costOfRoute; 
     }
 
-    public static Node calculateClosestNode(Set<Node> unvisited){
-        Node closestNode = null; 
-        int closestDistance = Integer.MAX_VALUE; 
-        for(Node node: unvisited){
-            int nodeDistance = node.getDistance();
-            if( nodeDistance< closestDistance)
-            {
-                closestDistance = nodeDistance;
-                closestNode = node; 
+    //Return the number of different routes between two towns with a distance of less than max distance
+    public int getRoutesLimitedDistance(Graph graph, String start, String end, int maxDistance){
+        calculateAllPaths(graph, start, end);
+        Map<Integer, List<String>> distancesAndRoutes = new HashMap<Integer, List<String>>(); 
+        for(String path: graph.allPathsBetweenTwoNodes){
+            path = (start.charAt(0)+path).trim();
+            int distance = graph.getDistanceAlongRoute(path); 
+            List<String> listOfRoutes = distancesAndRoutes.getOrDefault(distance, new ArrayList<String>());
+            listOfRoutes.add(path);
+            distancesAndRoutes.put(distance, listOfRoutes); 
+        }
+
+        Set<String> allUniqueRoutes = new HashSet<String>(); 
+        for(int route: distancesAndRoutes.keySet())
+        {
+            if(route <= maxDistance){
+                for(String path : distancesAndRoutes.get(route))
+                {
+                    allUniqueRoutes.add(path); 
+                }
+                // getVariations(distancesAndRoutes, allUniqueRoutes, route, maxDistance); 
             }
         }
-        return closestNode; 
+        // return allUniqueRoutes.size() graph.getVariations(distancesAndRoutes, allUniqueRoutes, maxDistance);
+        return allUniqueRoutes.size(); 
     }
 
-    public static void calculateShortestDistance(Graph graph, Node adjacentNode, Node source, int weight){
-        if(source.getDistance() + weight < adjacentNode.getDistance()){
-            adjacentNode.setDistance(source.getDistance()+weight);
-            //System.out.println(adjacentNode.getDistance());
-
-            Node temp = graph.getNode(String.valueOf(adjacentNode.id));
-            if(adjacentNode.getDistance() < temp.getDistance())
-            {
-                temp.setDistance(adjacentNode.getDistance());
-            }
-            LinkedList<Node> shortestPath = new LinkedList<>(source.getShortestPath()); 
-            shortestPath.add(source); 
-            adjacentNode.setShortestPath(shortestPath);
-        }
-    }
-    public static String dijkstrasAlgorithm(Graph graph, String start, String end)
+    // Return the number of different routes between two towns with a maximum number of allocated stops 
+    public int getRoutesLimitedStops(Graph graph, String start, String end, int maxNumberOfStops)
     {
-        Node startNode = graph.getNode(start); 
-        startNode.setDistance(0); 
-
-        Set<Node> visited = new HashSet<Node>(); 
-        Set<Node> unvisited = new HashSet<Node>(); 
-
-        unvisited.add(startNode); 
-        while(unvisited.size() > 0){
-            Node currentNode = calculateClosestNode(unvisited); 
-            unvisited.remove(currentNode);
-            for(Map.Entry<Node, Integer> adjacentNode: graph.getAdjacentVerticies(currentNode.id).entrySet()){
-                if(!visited.contains(adjacentNode.getKey())){
-                    // adjacentNode.getKey() = node
-                    // adjacentNode.getValue() = node weight 
-                    calculateShortestDistance(graph, adjacentNode.getKey(), currentNode, adjacentNode.getValue()); 
-                    unvisited.add(adjacentNode.getKey()); 
-                }
+        calculateAllPaths(graph, start, end);
+        int routes = 0; maxNumberOfStops++;
+        for(String path : graph.allPathsBetweenTwoNodes){
+            if(start == end) path = (start.charAt(0) + path).trim();
+            if(path.length() <= maxNumberOfStops){
+                routes++; 
+                // System.out.println(path + " " + path.length()); //< Debug: print out all paths that have number of stops <= maxNumberOfStops 
             }
-             visited.add(currentNode);    
-         }
-         int shortestDistance = graph.getNode(end).getDistance(); 
-         if(shortestDistance == Integer.MAX_VALUE) return("NO SUCH ROUTE").trim();
-        return String.valueOf(graph.getNode(end).getDistance());
+            // System.out.println(path + " " + path.length());  //<Debug: print all possible paths between two nodes 
+        }
+        return routes;  
     }
 
-        // Variation of depth first search to find the route 
-        public void search(Graph graph, String start, String end, List<Character> visited)
-        {
-            Map<Node, Integer> startNodes = graph.getAdjacentVerticies(visited.get(visited.size()-1)); 
-            for(Node node : startNodes.keySet()){
-                if(visited.contains(node.id)) continue;
-                if(node.id == end.charAt(0)){
-                    visited.add(node.id); 
-                    addPath(graph, visited); 
-                    visited.remove(visited.size() - 1); 
-                    break; 
-                }
-            }
-            for(Node node: startNodes.keySet()){
-                if(visited.contains(node.id) || node.id == end.charAt(0)){
-                    continue;
-                }
-                visited.add(node.id);
-                search(graph, start, end, visited); 
+
+    // Variation of depth first search to find the route 
+    public void search(Graph graph, String start, String end, List<Character> visited)
+    {
+        Map<Node, Integer> startNodes = graph.getAdjacentVerticies(visited.get(visited.size()-1)); 
+        for(Node node : startNodes.keySet()){
+            if(visited.contains(node.id)) continue;
+            if(node.id == end.charAt(0)){
+                visited.add(node.id); 
+                addPath(graph, visited); 
                 visited.remove(visited.size() - 1); 
+                break; 
             }
         }
-
-        public void addPath(Graph graph, List<Character> visited)
-        {
-            String path = "";
-            for(char node : visited){
-                path += String.valueOf(node);
+        for(Node node: startNodes.keySet()){
+            if(visited.contains(node.id) || node.id == end.charAt(0)){
+                continue;
             }
-            graph.allPathsBetweenTwoNodes.add(path.trim()); 
+            visited.add(node.id);
+            search(graph, start, end, visited); 
+            visited.remove(visited.size() - 1); 
         }
-
-
-        
-        public String calculateDistanceAlongRoute(String route)
-        {
-            int distanceAlongRoute  = getDistanceAlongRoute(route);
-            if(distanceAlongRoute == 0) return("NO SUCH ROUTE").trim();
-            else return (String.valueOf(distanceAlongRoute)).trim();
-        }
-
-
-        public void calculateAllPaths(Graph graph, String start, String end)
-        {
-            List<Character> visited = new ArrayList<Character>(); 
-            visited.add(start.charAt(0));
-
-            //if start node & end node are the same, get the start node's adjacent verticies and find all paths from 
-            // the start node's adjacent nodes to the end, and return those
-            if(start == end){
-                Map<Node, Integer> adjacentNodes = graph.getAdjacentVerticies(start.charAt(0));
-                for(Node node: adjacentNodes.keySet()){
-                    List<Character> visitedTemp = new ArrayList<Character>(); 
-                    visitedTemp.add(node.id);
-                    search(graph, String.valueOf(node.id), end, visitedTemp);
-                }; 
-            }
-            else{
-                search(graph, start, end, visited);
-            }
-        }
-
-        public int getRoutesWithLimitedPaths(Graph graph, String start, String end, int maxNumberOfStops)
-        {
-            calculateAllPaths(graph, start, end);
-            int routes = 0; maxNumberOfStops++;
-            for(String path : graph.allPathsBetweenTwoNodes){
-                if(start == end) path = (start.charAt(0) + path).trim();
-                if(path.length() < maxNumberOfStops){
-                    routes++; 
-                    // Debug: print out all paths that have number of stops <= maxNumberOfStops 
-                    // System.out.println(path + " " + path.len gth()); 
-                }
-                // Debug: print all possible paths between two nodes 
-                // System.out.println(path + " " + path.length());
-            }
-            return routes;  
-        }
-
-        public int getRoutesWithLimitedDistance(Graph graph, String start, String end, int maxDistance){
-            calculateAllPaths(graph, start, end);
-            Map<Integer, List<String>> distancesAndRoutes = new HashMap<Integer, List<String>>(); 
-            for(String path: graph.allPathsBetweenTwoNodes){
-                path = (start.charAt(0)+path).trim();
-                int distance = graph.getDistanceAlongRoute(path); 
-                List<String> listOfRoutes = distancesAndRoutes.getOrDefault(distance, new ArrayList<String>());
-                listOfRoutes.add(path);
-                distancesAndRoutes.put(distance, listOfRoutes); 
-            }
-
-            Set<String> allUniqueRoutes = new HashSet<String>(); 
-            for(int route: distancesAndRoutes.keySet())
-            {
-                if(route <= maxDistance){
-                    for(String path : distancesAndRoutes.get(route))
-                    {
-                        allUniqueRoutes.add(path); 
-                    }
-                    getVariations(distancesAndRoutes, allUniqueRoutes, route, maxDistance); 
-                }
-            }
-            // return allUniqueRoutes.size() graph.getVariations(distancesAndRoutes, allUniqueRoutes, maxDistance);
-            return -1; 
-        }
-
-        public int getVariations(Map<Integer, List<String>> distanceAndRoutes, Set<String> allUniqueRoutes, int route, int maxDistance)
-        {
-
-
-
-            return allUniqueRoutes.size(); 
-        }
+    }
 }
